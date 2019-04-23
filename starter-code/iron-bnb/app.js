8eac1e6afd9e47a22ff8347ff5f5cd90a51a9c1c
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-23 11:43:40 +0200

    commit RamaFer

--- starter-code/iron-bnb/app.js
+++ starter-code/iron-bnb/app.js
@@ -87,7 +87,7 @@ const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
 
 
-const products = require('./routes/Products');
+const products = require('./routes/products');
 app.use('/products', products);
       
 
--- starter-code/iron-bnb/routes/auth.js
+++ starter-code/iron-bnb/routes/auth.js
@@ -57,22 +57,14 @@ router.post("/signup", (req, res, next) => {
 });
 
 
-// GET /auth/google
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Google authentication will involve
-//   redirecting the user to google.com.  After authorization, Google
-//   will redirect the user back to this application at /auth/google/callback
+
 router.get('/facebook',
   passport.authenticate('facebook', 
   // { scope: ['https://www.googleapis.com/auth/plus.login'] }
   )
   )
 
-// GET /auth/google/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
+
 router.get('/facebook/callback', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
