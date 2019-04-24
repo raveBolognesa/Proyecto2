5a86883abc7ef95bd0bdc6d44e1744db62dd9ef5
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-24 18:55:30 +0200

    crea usuario con mas datos

--- models/modelProducts.js
+++ models/modelProducts.js
@@ -4,11 +4,19 @@ const Schema   = mongoose.Schema;
 const Productschema = new Schema({
   name: String,
   description: String,
-  rating: String,
+  kcal: Number,
   author: {type: Schema.Types.ObjectId, ref: 'User'},
   lat: Number,
   lng: Number,
   Pos: String,
+  typeFood: {
+            type: String, 
+            enum: ['Americana','China', 'Española', 'India', 'Italiana', 'Japonesa', 'Turca', 'Mexicana']
+            },  
+  vegan: Boolean,
+  veget: Boolean,
+  ingredients: Array,
+  comments: Array,
   imgPath: String,
   imgName: String
 }, {
